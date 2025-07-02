64ffb67adaf2a3793726db289f2924ab67ee76ef
asharaam1 <lohanaasharaam1@gmail.com>
2025-07-03 01:38:28 +0500

    learning

--- components/GoogleMapView.js
+++ components/GoogleMapView.js
@@ -1,34 +1,48 @@
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 import * as Location from 'expo-location';
-import { useEffect } from 'react';
-import { collection, doc, getDoc, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../Firbase/config';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+// import { collection, doc, getDoc, onSnapshot, query, where } from 'firebase/firestore';
+// import { db } from '../Firbase/config';
 
 
 const GoogleMapView = () => {
+  const [location, setLocation] = useState();
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  //! (()=>{})()       // This is a self-invoking function to avoid linting errors
+  //! function () {} // This is a regular function declaration
+  //! async function () {} // This is an async function declaration
 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
-        alert('Permission to access location was denied');
+        // alert('Permission to access location was denied');
+        setErrorMsg('Permission to access location was denied');
         return;
       }
     })();
     (async () => {
       let location = await Location.getCurrentPositionAsync({});
       console.log(location);
+      setLocation(location);
     })();
   }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   return (
     <View style={{ marginTop: 20 }}>
       <MapView
         style={{
-          width: Dimensions.get('screen').width * 0.89,
-          height: Dimensions.get('screen').width * 0.83,
+          width: Dimensions.get('screen').width * 0.99,
+          height: Dimensions.get('screen').height * 0.23,
           borderRadius: 20
         }}
         provider={PROVIDER_GOOGLE}
@@ -49,6 +63,9 @@ const GoogleMapView = () => {
           description="This user is here"
         />
       </MapView>
+      <View >
+        <Text style={{ textAlign: 'center', marginTop: 10 }}>{text}</Text>
+      </View>
     </View>
   )
 }
